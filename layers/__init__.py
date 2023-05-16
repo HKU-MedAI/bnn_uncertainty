c64eadc6e1175cefdbae3d5cb46f7249f359bf03
howar <hchanth@connect.hku.hk>
2023-05-16 23:09:48 +0800

    recorganization

--- layers/__init__.py
+++ layers/__init__.py
@@ -1,8 +1,4 @@
 from .BConv import BBBConv2d
 from .BLinear import BBBLinear
 from .flatten import FlattenLayer, ReverseFlattenLayer
-from .horseshoeConv import HorseshoeConvLayer
-from .horseshoe import HorseshoeLinearLayer
-from .r2d2Linear import R2D2LinearLayer
-from .r2d2Conv import R2D2ConvLayer
 from .resize import ResizeLayer
